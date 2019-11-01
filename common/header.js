14f589c8381214a27a1f24a1e70f41a550fd9689
joelpdurham <joeldur@gmail.com>
2019-11-01 09:26:08 -0700

    wrote header component

---
+++ common/header.js
@@ -0,0 +1,23 @@
+import Component from '../component.js';
+
+class Header extends Component {
+    renderHTML() {
+        return /*html*/`
+        <header>
+            <img id="pokedex" src="./assets/pokedex.png">
+            <span id="band">SPAN</span>
+            <div id="true-header">
+                <h1>Pokédex</h1>
+                <nav>
+                    <ul>
+                        <li>Nav 1</li>
+                        <li>Nav 2</li>
+                        <li>Nav 3</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>`;
+    }
+}
+
+export default Header;
